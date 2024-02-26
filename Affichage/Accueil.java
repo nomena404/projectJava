956ef7383096173f467996cc773576084ba23e5b
nomena404 <lovanome1@gmail.com>
2024-02-26 15:25:33 +0100

    debut implementation interface graphique

---
+++ Affichage/Accueil.java
@@ -0,0 +1,17 @@
+package Affichage;
+
+import javax.swing.*;
+
+public class Accueil extends JFrame {
+
+    public Accueil(){
+        super("Mon prémier lancement");
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
+
+    public static void main(String []argc){
+        Accueil a= new Accueil();
+    }
+}
